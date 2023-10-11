2637cbbbdb4b08562bfde16e4f4ece3c0e1dd304
Guilherme Barbosa <78804519+GuilhermeBarboosa@users.noreply.github.com>
2023-10-11 12:04:17 -0300

    feat grafico de pessoas doentes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import time
 import seaborn as sns
+import datetime
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -12,24 +13,57 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn import svm
 
-# Carregue os dados
+def limpa_string(string):
+  string = string.replace(" ", "")
+  string = string.replace(":", "-")
+
+  index = string.find(".")
+  if index != -1:
+      return string[:index]
+  else:
+      return string
+
+graficos = []
+nomeGraficos = []
+
+
 data = pd.read_csv("heart.csv")
 X = data.drop('Heart Disease', axis=1)
 y = data['Heart Disease']
 
-plt.figure(figsize=(10, 6))
+# Grafico de outliers
+graficos.append(plt.figure(figsize=(10, 6)))
 sns.boxplot(data=X)
 plt.title('Gráfico de Outliers Dataset')
 plt.xticks(rotation=90)
 plt.show()
+nomeGraficos.append("Matriz de Outliers")
 # Obtenha o número de dados aproveitados
 n_dados = len(data)
-
 print("Quantidade de dados utilizados: " + str(n_dados))
+# =================================================================================================
+
+#Grafico de quantidade de pessoas doentes
+heart_disease_counts = data['Heart Disease'].value_counts()
+graficos.append(plt.figure(figsize=(8, 6)))
+bars = plt.bar(heart_disease_counts.index, heart_disease_counts.values, color=['lightblue', 'lightcoral'])
+plt.xticks([0, 1], ['Sem Problemas Cardíacos', 'Com Problemas Cardíacos'])
+plt.xlabel('Condição Cardíaca')
+plt.ylabel('Número de Pessoas')
+plt.title('Distribuição de Pessoas com e sem Problemas Cardíacos')
+
+# Adicione os valores na legenda
+for bar, count in zip(bars, heart_disease_counts.values):
+    plt.text(bar.get_x() + bar.get_width() / 2, count, str(count), ha='center', va='bottom')
+
+plt.show()
+nomeGraficos.append("Distribuição de Pessoas com e sem Problemas Cardíacos")
+# =================================================================================================
+
 
 # KNN 1
 start_time = time.time()
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
@@ -44,13 +78,14 @@ print('Tempo de execução do modelo KNN 1: {:.2f} segundos'.format(elapsed_time
 print('Acurácia do modelo KNN 1: {:.2f}%'.format(accuracy * 100))
 print("\n")
 # print(classification_report(y_test, y_pred))
+graficos.append(plt.figure(figsize=(6, 4)))
 confusion = confusion_matrix(y_test, y_pred)
-plt.figure(figsize=(6, 4))
 sns.heatmap(confusion, annot=True, fmt='d', cmap='Blues')
 plt.xlabel('Valores Preditos')
 plt.ylabel('Valores Reais')
 plt.title('Matriz de Confusão do KNN 1')
 plt.show()
+nomeGraficos.append("Matriz de Confusão do KNN 1")
 # =================================================================================================
 
 # KNN 2
@@ -65,18 +100,19 @@ print('Tempo de execução do modelo KNN 2: {:.2f} segundos'.format(elapsed_time
 print('Acurácia do modelo KNN 2: {:.2f}%'.format(accuracy * 100))
 print("\n")
 confusion = confusion_matrix(y, predictions)
-plt.figure(figsize=(6, 4))
+graficos.append(plt.figure(figsize=(6, 4)))
 sns.heatmap(confusion, annot=True, fmt='d', cmap='Blues')
 plt.xlabel('Valores Preditos')
 plt.ylabel('Valores Reais')
 plt.title('Matriz de Confusão do KNN 2')
 plt.show()
+nomeGraficos.append("Matriz de Confusão do KNN 2")
 # =================================================================================================
 
 
 # SVM 1
 start_time = time.time()
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
@@ -91,12 +127,13 @@ print('Acurácia do modelo SVM 1: {:.2f}%'.format(accuracy * 100))
 print("\n")
 # print(classification_report(y_test, y_pred))
 confusion = confusion_matrix(y_test, y_pred)
-plt.figure(figsize=(6, 4))
+graficos.append(plt.figure(figsize=(6, 4)))
 sns.heatmap(confusion, annot=True, fmt='d', cmap='Blues')
 plt.xlabel('Valores Preditos')
 plt.ylabel('Valores Reais')
 plt.title('Matriz de Confusão do SVM 1')
 plt.show()
+nomeGraficos.append("Matriz de Confusão do SVM 1")
 # =================================================================================================
 
 # SVM 2
@@ -113,18 +150,19 @@ print('Tempo de execução do modelo SVM 2: {:.2f} segundos'.format(elapsed_time
 print('Acurácia do modelo SVM 2: {:.2f}%'.format(accuracy * 100))
 print("\n")
 confusion = confusion_matrix(y_test, y_pred)
-plt.figure(figsize=(6, 4))
+graficos.append(plt.figure(figsize=(6, 4)))
 sns.heatmap(confusion, annot=True, fmt='d', cmap='Blues')
 plt.xlabel('Valores Preditos')
 plt.ylabel('Valores Reais')
 plt.title('Matriz de Confusão do SVM 2')
 plt.show()
+nomeGraficos.append("Matriz de Confusão do SVM 2")
 # =================================================================================================
 
 
 # Redes Neurais Artificiais 1
 start_time = time.time()
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
@@ -139,12 +177,13 @@ print('Acurácia do modelo RNA 1: {:.2f}%'.format(accuracy * 100))
 print("\n")
 # print(classification_report(y_test, y_pred))
 confusion = confusion_matrix(y_test, y_pred)
-plt.figure(figsize=(6, 4))
+graficos.append(plt.figure(figsize=(6, 4)))
 sns.heatmap(confusion, annot=True, fmt='d', cmap='Blues')
 plt.xlabel('Valores Preditos')
 plt.ylabel('Valores Reais')
 plt.title('Matriz de Confusão do RNA 1')
 plt.show()
+nomeGraficos.append("Matriz de Confusão do RNA 1")
 # =================================================================================================
 
 
@@ -162,19 +201,20 @@ print('Tempo de execução do modelo RNA 2: {:.2f} segundos'.format(elapsed_time
 print('Acurácia do modelo RNA 2: {:.2f}%'.format(accuracy * 100))
 print("\n")
 confusion = confusion_matrix(y_test, y_pred)
-plt.figure(figsize=(6, 4))
+graficos.append(plt.figure(figsize=(6, 4)))
 sns.heatmap(confusion, annot=True, fmt='d', cmap='Blues')
 plt.xlabel('Valores Preditos')
 plt.ylabel('Valores Reais')
 plt.title('Matriz de Confusão do RNA 2')
 plt.show()
+nomeGraficos.append("Matriz de Confusão do RNA 2")
 # =================================================================================================
 
 
 
 #Regressão logistica 1
 start_time = time.time()
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
@@ -189,12 +229,13 @@ print('Acurácia do modelo Regressão logistica 1: {:.2f}%'.format(accuracy * 10
 print("\n")
 # print(classification_report(y_test, y_pred))
 confusion = confusion_matrix(y_test, y_pred)
-plt.figure(figsize=(6, 4))
+graficos.append(plt.figure(figsize=(6, 4)))
 sns.heatmap(confusion, annot=True, fmt='d', cmap='Blues')
 plt.xlabel('Valores Preditos')
 plt.ylabel('Valores Reais')
 plt.title('Matriz de Confusão da Regressão Logística 1')
 plt.show()
+nomeGraficos.append("Matriz de Confusão do Regressão Logística 1")
 # =================================================================================================
 
 
@@ -212,10 +253,25 @@ print('Tempo de execução do modelo Regressão logistica 2: {:.2f} segundos'.fo
 print('Acurácia do modelo Regressão logistica 2: {:.2f}%'.format(accuracy * 100))
 print("\n")
 confusion = confusion_matrix(y_test, y_pred)
-plt.figure(figsize=(6, 4))
+graficos.append(plt.figure(figsize=(6, 4)))
 sns.heatmap(confusion, annot=True, fmt='d', cmap='Blues')
 plt.xlabel('Valores Preditos')
 plt.ylabel('Valores Reais')
 plt.title('Matriz de Confusão da Regressão Logística 1')
 plt.show()
+nomeGraficos.append("Matriz de Confusão do Regressão Logística 2")
 # =================================================================================================
+
+for grafico in graficos:
+    # hora_atual = limpa_string(str(datetime.datetime.now()))
+
+    # titulo_da_janela = nomeGraficos[graficos.index(grafico)] + "-" + str(hora_atual)
+
+    titulo_da_janela = nomeGraficos[graficos.index(grafico)]
+    # Obtém o nome do arquivo
+    nome_arquivo = "grafico_" + titulo_da_janela + ".png"
+
+    # Salva o gráfico
+    grafico.savefig(f"graficos/{nome_arquivo}")
+
+
